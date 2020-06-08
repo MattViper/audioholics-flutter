813a25c8f3c7ca2d4cb3da182e85a4c799342bc4
MattViper <mattoshinigami@gmail.com>
2020-06-08 22:41:27 +0200

    Init Sign up artist name screen

--- lib/screens/sign_up_artist_name_screen.dart
+++ lib/screens/sign_up_artist_name_screen.dart
@@ -1,5 +1,5 @@
-
-
+import 'package:audioholics/shared/color_palette.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignUpArtistNameScreen extends StatelessWidget {
@@ -7,7 +7,73 @@ class SignUpArtistNameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.indigoAccent, ColorPalette.PrimaryColor],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 300,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Text(
+                'What\'s your artist name?',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 24.0,
+                    color: Colors.white),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: TextFormField(
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontStyle: FontStyle.italic),
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                    labelText: 'Your artist name',
+                    labelStyle:
+                        TextStyle(color: Colors.white70, fontSize: 18.0),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      style: BorderStyle.none,
+                    )),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      style: BorderStyle.none,
+                    ))),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(200, 100, 10, 0),
+              child: FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Continue',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
-
 }
